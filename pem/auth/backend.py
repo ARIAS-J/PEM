a6b84b2a4cb8e0a88242cd52ea2f405042ee1f6d
No-Hex <evilheroe1@gmail.com>
2022-01-09 16:58:44 -0400

    .

--- pem/auth/backend.py
+++ pem/auth/backend.py
@@ -5,14 +5,14 @@ from pem.models import User
 class CustomBackend(ModelBackend):
     def authenticate(self, request, **kwargs):
         email = kwargs['email']
-        print('Aqui esta el email backend ==>>', email)
+        
         password = kwargs['password']
-        print('Aqui esta el password backend ==>>', password)
+        
         try:
             customer = User.objects.get(email=email)
-            print('GET query aqui ===>>', customer)
+            
             password_isvalid = customer.check_password(password)
-            print('aqui esta el password check ===>>', password_isvalid)
+            
             if password_isvalid:
                 
                 return customer
