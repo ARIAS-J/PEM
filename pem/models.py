87aa0f973582d99f39365db0cca0ace24a103468
No-Hex <evilheroe1@gmail.com>
2022-02-17 10:21:24 -0400

    modifi

--- pem/models.py
+++ pem/models.py
@@ -57,24 +57,4 @@ class User(AbstractUser):
         return self.email
 
 
-class Categoria(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, name='id')
-    nombre = models.CharField(max_length=250, null=True,)
-    
-    
-    #Relationship
-    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True)
-    
-    def __str__(self):
-        return self.nombre
 
-
-class Registro(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, name='id')
-    monto = models.DecimalField(max_digits=9, decimal_places=2, null=True)
-    descripcion = models.CharField(max_length=1000, null=True)
-    fecha = models.CharField(max_length=250, null=True)
-
-    #Relationship
-    user = models.ForeignKey("User", on_delete=models.CASCADE, null=True, blank=True)
-    categoria = models.ForeignKey("Categoria", on_delete=models.CASCADE, null=True, blank=True)
